731ecaa26a40301487c7427548479cb677442f38
billbo24 <79063296+billbo24@users.noreply.github.com>
2023-06-12 10:17:11 -0400

    Update ContentView.swift
    Changing to stash 0

--- GITHUB_TEST_STUFF/ContentView.swift
+++ GITHUB_TEST_STUFF/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Github is dumb and I hate it")
             Text("The quick brown fox jumped over the lazy dog")
-            Text("Base case")
+            Text("Going to call this stash 0")
             
         }
         .padding()
