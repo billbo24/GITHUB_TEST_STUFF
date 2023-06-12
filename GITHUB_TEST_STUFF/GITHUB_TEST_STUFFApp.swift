6c862c1bc3b5b6d3fb75413c9d941b75d3b07e8f
billbo24 <wfloyd1231@gmail.com>
2023-06-12 09:53:39 -0400

    Initial Commit

---
+++ GITHUB_TEST_STUFF/GITHUB_TEST_STUFFApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHUB_TEST_STUFFApp.swift
+//  GITHUB_TEST_STUFF
+//
+//  Created by William Floyd on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GITHUB_TEST_STUFFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
